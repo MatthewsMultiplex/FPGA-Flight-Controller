b26c751425854ed9fbc9da25cf298775762238fe
MatthewsMultiplex <matthewbpoffengineering@gmail.com>
2025-08-16 16:32:53 -0400

    Update PWM_Gen1.v

--- PWM_Gen1.v
+++ PWM_Gen1.v
@@ -1,30 +1,23 @@
-`timescale 1ns/ 1ps
+// filename: PWM_Gen.v
+`timescale 1ns/1ps
 
-module PWM_Gen(
-    input clk,
-    input reset, 
-    input [7:0] duty_cycle,
-    output pwm_out
-    );
- 
-parameter CLK_FREQ = 100000000;
-parameter PWM_FREQ = 20000;
+module PWM_Gen #(
+    parameter integer CLK_HZ = 125_000_000,
+    parameter integer PWM_HZ = 20_000
+)(
+    input  wire       clk,
+    input  wire       reset,
+    input  wire [7:0] duty_cycle, // 0..255
+    output wire       pwm_out
+);
+    localparam integer MAX_COUNT = CLK_HZ / PWM_HZ;
+    reg [31:0] counter;
 
-localparam integer MAX_COUNT = CLK_FREQ / PWM_FREQ;   
+    always @(posedge clk) begin
+        if (reset) counter <= 0;
+        else if (counter >= MAX_COUNT-1) counter <= 0;
+        else counter <= counter + 1;
+    end
 
-reg [31:0] counter = 0; 
-
-always @ (posedge clk) begin
-
-    if (reset)
-            counter <= 0; 
-    else if (counter >= MAX_COUNT - 1)
-            counter <= 0;
-    else 
-            counter <= counter + 1; 
-
-end
-
-assign pwm_out = (counter < ((duty_cycle * MAX_COUNT) >> 8)) ? 1'b1 : 1'b0;
-
+    assign pwm_out = (counter < ((duty_cycle * MAX_COUNT) >> 8)) ? 1'b1 : 1'b0;
 endmodule
